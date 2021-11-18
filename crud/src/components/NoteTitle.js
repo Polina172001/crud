da9c0db9295a898d661e4bfe315882f93c223bb5
Polina <polina17012001@mail.ru>
2021-11-19 00:36:56 +0300

    version1

---
+++ crud/src/components/NoteTitle.js
@@ -0,0 +1,16 @@
+const NoteTitle = ({updateNotes}) => {
+  return (
+    <header className="mb-5">
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+              Notes
+          </a>
+          <button className="btn btn-primary ml-auto" onClick={updateNotes}>Обновить</button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NoteTitle;
